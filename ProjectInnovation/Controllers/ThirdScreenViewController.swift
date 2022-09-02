ecc65917fcc36199acf6f74f56f8b4cf044ea12d
SanskarGautam <Sanskargautam1603@gmail.com>
2022-09-03 02:46:41 +0530

    Revert "taskscreen"
    This reverts commit 92f1d7e1a814a9ffa8f1ce11274cd348aabda5e3.

--- ProjectInnovation/Controllers/ThirdScreenViewController.swift
+++ ProjectInnovation/Controllers/ThirdScreenViewController.swift
@@ -17,7 +17,7 @@ class ThirdScreenViewController: UIViewController {
 
     @IBAction func btnClicked(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "TaskScreenViewController") as! TaskScreenViewController
+        let vc = storyboard.instantiateViewController(withIdentifier: "AssignedMeTaskViewController") as! AssignedMeTaskViewController
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
