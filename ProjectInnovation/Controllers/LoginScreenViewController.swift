1dc4b0715305b2c802c272fceca226f9ffa6bedd
SanskarGautam <Sanskargautam1603@gmail.com>
2022-09-03 10:02:26 +0530

    login changes

--- ProjectInnovation/Controllers/LoginScreenViewController.swift
+++ ProjectInnovation/Controllers/LoginScreenViewController.swift
@@ -9,29 +9,24 @@ import UIKit
 
 class LoginScreenViewController: UIViewController {
 
-    @IBOutlet weak var password: UITextField!
-    @IBOutlet weak var emailId: UITextField!
-    
-    @IBAction func signinBtnClicked(_ sender: Any) {
+    @IBAction func slackButton(_ sender: Any) {
         let vc = storyboard?.instantiateViewController(withIdentifier: "TaskScreenViewController") as! TaskScreenViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func googleButton(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "MainScreenViewController") as! MainScreenViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func microsoftButton(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "SecondScreenViewController") as! SecondScreenViewController
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
